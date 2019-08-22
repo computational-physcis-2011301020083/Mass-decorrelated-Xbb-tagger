9e2d5b641766dcdd2122b39ae2ed60611e689f93
computational-physcis-2011301020083 <2432001232@qq.com>
2019-08-22 14:03:12 -0700

    Step two: flatten and split samples

--- process/run.sh
+++ process/run.sh
@@ -1,9 +1,11 @@
+#Flatten samples
 for i in $(ls ../DataVRGhost/MergedData/*.h5)
 do
 	path=$i
 	python flatten.py  --path $path
 done
 
+#Split samples into training validation and testing
 for i in $(ls ../DataVRGhost/FlattenData/*.h5)
 do      
         path=$i
