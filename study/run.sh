7c140adb5f857e7df4ef8be50832ab77e14046ad
computational-physcis-2011301020083 <2432001232@qq.com>
2019-08-23 13:44:04 -0700

    Study

--- study/run.sh
+++ study/run.sh
@@ -4,3 +4,12 @@ python predict.py --path $pathToModel
 #ROC study
 python rocRatio.py --path $pathToPredictionfile
 
+#Calculate the threshold corresponding to a certain signal efficiency
+python calculateThresh.py --path $pathToPredictionfile 
+
+#JSD study
+python jsd.py --path $pathToPredictionfile --thresh $threshold
+
+#Jet mass distribution
+python jetmass.py --path $pathToPredictionfile --thresh $threshold --eff $SignalEff
+
